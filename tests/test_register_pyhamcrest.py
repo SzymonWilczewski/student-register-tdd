7400d319a76ac83810504a02e8d1892fe0908a5d
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2020-12-03 23:36:41 +0100

    RED test_add_student pyhamcrest

---
+++ tests/test_register_pyhamcrest.py
@@ -0,0 +1,22 @@
+import unittest
+from hamcrest import *
+
+from register.register import *
+
+
+class TestRegisterPyHamcrest(unittest.TestCase):
+
+    def setUp(self):
+        self.register = Register()
+
+    def test_add_student(self):
+        expected = ["f53b878e-7105-4f44-9f88-f1cc5d8b63f5", "Anna", "Nowak", 1]
+        actual = self.register.add_student("f53b878e-7105-4f44-9f88-f1cc5d8b63f5", "Anna", "Nowak", 1)
+        assert_that(actual, equal_to(expected))
+
+    def tearDown(self):
+        self.register = None
+
+
+if __name__ == '__main__':
+    unittest.main()
