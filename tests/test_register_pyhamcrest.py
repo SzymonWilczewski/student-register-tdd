0d6b6cf6f66bdb51de541d8560e213061ece5e66
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2020-12-03 23:52:37 +0100

    RED test_edit_student pyhamcrest

--- tests/test_register_pyhamcrest.py
+++ tests/test_register_pyhamcrest.py
@@ -14,6 +14,12 @@ class TestRegisterPyHamcrest(unittest.TestCase):
         actual = self.register.add_student("f53b878e-7105-4f44-9f88-f1cc5d8b63f5", "Anna", "Nowak", 1)
         assert_that(actual, equal_to(expected))
 
+    def test_edit_student(self):
+        expected = [2]
+        actual = self.register.edit_student("dc338aff-d851-4c08-a319-ed4e18640b36", new_first_name=None,
+                                            new_last_name=None, new_year=2)
+        assert_that(actual, equal_to(expected))
+
     def tearDown(self):
         self.register = None
 
