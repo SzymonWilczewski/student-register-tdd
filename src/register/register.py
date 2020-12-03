55265b06f03f96e7cc444a88dc36c5b5ece232d6
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2020-12-03 23:25:30 +0100

    Implement Student class

--- src/register/register.py
+++ src/register/register.py
@@ -4,5 +4,14 @@ class Register:
 
 
 class Student:
-    def __init__(self):
-        pass
+    def __init__(self, id_, first_name, last_name, year, subjects=None, comments=None):
+        if subjects is None:
+            subjects = []
+        if comments is None:
+            comments = []
+        self.id_ = id_
+        self.first_name = first_name
+        self.last_name = last_name
+        self.year = year
+        self.subjects = subjects
+        self.comments = comments
