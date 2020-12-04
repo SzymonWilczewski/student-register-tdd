8257332b52d594b4f4c8aa30ae8926b20db1805e
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2020-12-04 12:26:47 +0100

    RED test_add_grade assertpy

--- tests/test_register_assertpy.py
+++ tests/test_register_assertpy.py
@@ -45,6 +45,11 @@ class TestRegisterAssertpy(unittest.TestCase):
         actual = self.register.remove_subject("dc338aff-d851-4c08-a319-ed4e18640b36", 0)
         assert_that(actual).is_equal_to(expected)
 
+    def test_add_grade(self):
+        expected = 2.5
+        actual = self.register.add_grade("dc338aff-d851-4c08-a319-ed4e18640b36", 0, 2.5)
+        assert_that(actual).is_equal_to(expected)
+
     def tearDown(self):
         self.register = None
 
