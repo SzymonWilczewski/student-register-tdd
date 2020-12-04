102143eca8b63278fb5d07e29fbf8ed3da7cb432
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2020-12-04 16:15:10 +0100

    RED test_add_grade_is_greater_than_2 unittest

--- tests/test_register_unittest.py
+++ tests/test_register_unittest.py
@@ -49,6 +49,10 @@ class TestRegisterUnittest(unittest.TestCase):
         actual = self.register.add_grade("dc338aff-d851-4c08-a319-ed4e18640b36", 0, 2.5)
         self.assertEqual(expected, actual)
 
+    def test_add_grade_is_greater_than_2(self):
+        actual = self.register.add_grade("dc338aff-d851-4c08-a319-ed4e18640b36", 0, 2.5)
+        self.assertGreater(actual, 2)
+
     def test_edit_grades(self):
         expected = [5, 2.5, 3, 1.5, 1]
         actual = self.register.edit_grades("dc338aff-d851-4c08-a319-ed4e18640b36", 0, [5, 2.5, 3, 1.5, 1])
