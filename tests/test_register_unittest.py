4a48da61f5f761233e759df0bfadd264b6987de0
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2020-12-04 14:13:04 +0100

    RED test_edit_student_exception unittest

--- tests/test_register_unittest.py
+++ tests/test_register_unittest.py
@@ -82,6 +82,11 @@ class TestRegisterUnittest(unittest.TestCase):
         with self.assertRaises(TypeError):
             self.register.add_student("f53b878e-7105-4f44-9f88-f1cc5d8b63f5", "Anna", "Nowak", "1")
 
+    def test_edit_student_exception(self):
+        with self.assertRaises(TypeError):
+            self.register.edit_student("dc338aff-d851-4c08-a319-ed4e18640b36", new_first_name=None,
+                                       new_last_name=None, new_year="2")
+
     def tearDown(self):
         self.register = None
 
