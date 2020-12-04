860862f2c696f830f4ef6e163d43fec9ccf8df34
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2020-12-04 16:40:52 +0100

    RED test_add_comment_contains_U unittest

--- src/register/register.py
+++ src/register/register.py
@@ -120,7 +120,7 @@ class Register:
         for student in self.db:
             if student.id_ == id_:
                 student.comments.append(comment)
-                return comment
+                return "uczeń przeszkadza w prowadzeniu zajęć"
 
     def edit_comment(self, id_, index, new_comment):
         if type(id_) != str or type(index) != int or type(new_comment) != str:
--- tests/test_register_unittest.py
+++ tests/test_register_unittest.py
@@ -86,6 +86,11 @@ class TestRegisterUnittest(unittest.TestCase):
                                            "Uczeń przeszkadza w prowadzeniu zajęć")
         self.assertEqual(expected, actual)
 
+    def test_add_comment_contains_U(self):
+        actual = self.register.add_comment("dc338aff-d851-4c08-a319-ed4e18640b36",
+                                           "Uczeń przeszkadza w prowadzeniu zajęć")
+        self.assertRegex(actual, "U")
+
     def test_edit_comment(self):
         expected = "Uczeń przeszkadza w prowadzeniu zajęć"
         actual = self.register.edit_comment("dc338aff-d851-4c08-a319-ed4e18640b36", 0,
