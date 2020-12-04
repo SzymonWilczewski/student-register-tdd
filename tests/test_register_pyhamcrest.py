ebcd5314b459802954a05d621f772522ab391a4e
Szymon Wilczewski <szymonkarolwilczewski@gmail.com>
2020-12-04 16:51:59 +0100

    RED test_edit_comment_doesnt_contain_q pyhamcrest

--- tests/test_register_pyhamcrest.py
+++ tests/test_register_pyhamcrest.py
@@ -98,6 +98,11 @@ class TestRegisterPyHamcrest(unittest.TestCase):
                                             "Uczeń przeszkadza w prowadzeniu zajęć")
         assert_that(actual, equal_to(expected))
 
+    def test_edit_comment_doesnt_contain_q(self):
+        actual = self.register.edit_comment("dc338aff-d851-4c08-a319-ed4e18640b36", 0,
+                                            "Uczeń przeszkadza w prowadzeniu zajęć")
+        assert_that(actual, is_not(contains_string("q")))
+
     # EXCEPTIONS
 
     def test_add_student_exception(self):
